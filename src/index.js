5653c2c478c3cc2d37c2d93d8ef7911b10af3afb
dynilath <dynilath@qq.com>
2025-02-21 19:18:53 +0800

    refactor: add index.js to export core modules

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import AssetManager from "./AssetManager";
+import ActivityManager from "./ActivityManager";
+import ChatRoomOrder from "./ChatRoomOrder";
+import ModManager from "./ModManager";
+import { Tools } from "./Tools";
+import { VersionSupport } from "./VersionSupport";
+import { Path } from "./path";
+
+export { AssetManager, ActivityManager, ChatRoomOrder, ModManager, Tools, VersionSupport, Path };
