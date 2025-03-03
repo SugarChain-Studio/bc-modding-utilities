b8258a87ac28f41cee533850ceb0a718ebd17519
dynilath <dynilath@qq.com>
2025-03-03 21:27:50 +0800

    feat: add DrawCharacterModifier for customizable character drawing offsets

---
+++ src/ChatRoomOrder/drawOffset.js
@@ -0,0 +1,45 @@
+import ModManager from "@mod-utils/ModManager";
+
+/**
+ * @typedef { Object } DrawOffsetParam
+ * @property { number } X
+ * @property { number } Y
+ * @property { number } Zoom
+ */
+
+/**
+ * @typedef { (from:DrawOffsetParam)=>DrawOffsetParam } DrawOffsetFunction
+ */
+
+const DrawOffsetInstanceName = "ECHODrawOffsetInstance";
+
+/** @type {DrawOffsetFunction[]} */
+const modifiers = []
+
+export class DrawCharacterModifier {
+    static init() {
+        if (window[DrawOffsetInstanceName]) return;
+        window[DrawOffsetInstanceName] = DrawCharacterModifier;
+
+        ModManager.progressiveHook("DrawCharacter", 1)
+            .inside("ChatRoomCharacterViewLoopCharacters")
+            .inject((args, next) => {
+                const [C, X, Y, Zoom] = args;
+                let result = { X, Y, Zoom };
+                for (const modifier of modifiers) {
+                    result = modifier(result);
+                }
+
+                args[1] = result.X;
+                args[2] = result.Y;
+                args[3] = result.Zoom;
+            });
+    }
+
+    /**
+     * @param {DrawOffsetFunction} modifier 
+     */
+    static addModifier(modifier) {
+        modifiers.push(modifier);
+    }
+}
