7f8d0b65e94b996c79674459b1b47c44281496e3
dynilath <dynilath@qq.com>
2025-03-04 12:49:29 +0800

    refactor: update CharacterTag structure to use CharacterTagItem for enhanced tagging

--- src/charaTag.js
+++ src/charaTag.js
@@ -6,14 +6,20 @@ const ECHO_INFO_TAG = "ECHO_INFO";
 const GLOBAL_INSTANCE_TAG = "ECHO_CHARA_TAG";
 
 /**
- * @typedef { Record<string,string> } CharacterTagInfo
+ * @typedef {Object}  CharacterTagItem
+ * @property {string} version
+ * @property {boolean} beta
+ */
+
+/**
+ * @typedef { Record<string,CharacterTagItem> } CharacterTagInfo
  */
 
 /**
  * @param {CharacterTagInfo} localTag
  * @param {number} [target]
  */
-function sendMyTag(localTag, target) {
+function sendFunc(localTag, target) {
     ServerSend("ChatRoomChat", {
         Content: ECHO_INFO_TAG,
         Type: "Hidden",
@@ -27,86 +33,119 @@ function sendMyTag(localTag, target) {
     });
 }
 
-/**
- * @param {ServerChatRoomMessage} data
- */
-function processOtherCharaTag(data) {
-    if (data.Type !== "Hidden") return;
-    if (data.Content !== ECHO_INFO_TAG) return;
-    if (!Array.isArray(data.Dictionary)) return;
-
-    const receivedTag = /** @type {any[]}*/ (data.Dictionary).find((d) => d.Type === ECHO_INFO_TAG);
-    if (!receivedTag) return;
-    const { Content } = receivedTag;
-
-    const fromChara = ChatRoomCharacter.find((c) => c.MemberNumber === data.Sender);
-    if (!fromChara) return;
-    fromChara[ECHO_INFO_TAG] = Content;
-}
-
-export class CharacterTag {
+export class CharacterTagInstance {
     /** @type {CharacterTagInfo} */
     localTag = {}; // 本地标签
     hooked = false;
+    version = 1;
 
-    constructor() { }
+    constructor() {}
+
+    migrate(old) {
+        this.localTag = old["localTag"] || {};
+        this.hooked = !!old["hooked"];
+    }
 
     /**
-     * @returns {CharacterTag}
+     * @param {string} name
+     * @param {CharacterTagItem} tag
      */
-    static instance() {
-        if (!globalThis[GLOBAL_INSTANCE_TAG]) {
-            globalThis[GLOBAL_INSTANCE_TAG] = new CharacterTag();
+    tag(name, tag) {
+        this.localTag[name] = tag;
+
+        const tagPlayer = () => {
+            Player[ECHO_INFO_TAG] = {
+                ...Player[ECHO_INFO_TAG],
+                [name]: tag,
+            };
+        };
+
+        ModManager.afterPlayerLogin(() => {
+            tagPlayer();
+        });
+    }
+
+    /**
+     * @param {number} [memberNumber]
+     */
+    send(memberNumber) {
+        sendFunc(this.localTag, memberNumber);
+    }
+
+    /**
+     * @param {ServerChatRoomMessage} data
+     */
+    parse(data) {
+        if (data.Type !== "Hidden") return;
+        if (data.Content !== ECHO_INFO_TAG) return;
+        if (!Array.isArray(data.Dictionary)) return;
+
+        const receivedTag = /** @type {any[]}*/ (data.Dictionary).find((d) => d.Type === ECHO_INFO_TAG);
+        if (!receivedTag) return;
+        const { Content } = receivedTag;
+
+        const fromChara = ChatRoomCharacter.find((c) => c.MemberNumber === data.Sender);
+        if (!fromChara) return;
+        fromChara[ECHO_INFO_TAG] = Content;
+    }
+
+    /**
+     * @param {Character} C
+     * @param {string} key
+     * @returns {CharacterTagItem | undefined}
+     */
+    get(C, key) {
+        return C[ECHO_INFO_TAG]?.[key];
+    }
+}
+
+export class CharacterTag {
+    /**
+     * @returns {CharacterTagInstance}
+     */
+    static get instance() {
+        const oldGlobal = globalThis[GLOBAL_INSTANCE_TAG];
+        const newGlobal = new CharacterTagInstance();
+
+        if (!oldGlobal || !(oldGlobal instanceof CharacterTagInstance)) globalThis[GLOBAL_INSTANCE_TAG] = newGlobal;
+        else if (!oldGlobal.version || oldGlobal.version < newGlobal.version) {
+            newGlobal.migrate(oldGlobal);
+            globalThis[GLOBAL_INSTANCE_TAG] = newGlobal;
         }
+
         return globalThis[GLOBAL_INSTANCE_TAG];
     }
 
     /**
      * @param {string} name
-     * @param {string} version
+     * @param {CharacterTagItem} tag
      */
-    static myTag(name, version) {
-        this.instance().localTag[name] = version;
-
-        const tagPlayer = () => {
-            Player[ECHO_INFO_TAG] = {
-                ...Player[ECHO_INFO_TAG],
-                [name]: version,
-            };
-        };
-
-        if (Player && Player.MemberNumber) {
-            tagPlayer();
-        } else {
-            ModManager.progressiveHook("LoginResponse", 10)
-                .next()
-                .inject((args, next) => {
-                    if (typeof args[0] !== "string") tagPlayer();
-                });
-        }
+    static tag(name, tag) {
+        this.instance.tag(name, tag);
     }
 
     static init() {
-        const instance = this.instance();
+        const instance = this.instance;
         if (instance.hooked) return;
         instance.hooked = true;
 
         ModManager.progressiveHook("ChatRoomSyncMemberJoin", 10).inject((args, next) => {
-            sendMyTag(this.instance().localTag, args[0].SourceMemberNumber);
+            this.instance.send(args[0].SourceMemberNumber);
         });
 
         ModManager.progressiveHook("ChatRoomSync", 10).inject((args, next) => {
-            sendMyTag(this.instance().localTag);
+            this.instance.send();
         });
 
-        ChatRoomEvents.on("Hidden", (data) => processOtherCharaTag(data));
+        ChatRoomEvents.on("Hidden", (data) => this.instance.parse(data));
     }
 
     /**
      * @param {Character} chara
-     * @returns { CharacterTagInfo | undefined }
+     * @param {string} key
+     * @returns { CharacterTagItem | undefined }
      */
-    static getTag(chara) {
-        return chara[ECHO_INFO_TAG];
+    static get(chara, key) {
+        return this.instance.get(chara, key);
     }
 }
