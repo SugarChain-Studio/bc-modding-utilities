4b44bf6ff0584b87e1ac9a2f493681d10fa3f887
dynilath <dynilath@qq.com>
2025-03-20 13:01:19 +0800

    refactor: update package dependencies and enhance type definitions

--- src/Typedef.d.ts
+++ src/Typedef.d.ts
@@ -10,14 +10,32 @@ declare const __mod_beta_flag__: boolean;
 declare const __mod_rollup_imports__: string[];
 declare const __mod_rollup_setup__: string[];
 
+/** 扩展的身体组（非物品）名称 */
+type CustomGroupBodyName =
+    | AssetGroupBodyName
+    | `${AssetGroupBodyName}_笨笨蛋Luzi`
+    | `${AssetGroupBodyName}_笨笨笨蛋Luzi2`
+    | 'Liquid2_Luzi'
+    | 'BodyMarkings2_Luzi'
+    | '动物身体_Luzi'
+    | '长袖子_Luzi'
+    | '新前发_Luzi'
+    | '新后发_Luzi'
+    | '额外头发_Luzi'
+    | '额外身高_Luzi'
+    | '身体痕迹_Luzi'
+    | '眼睛左_Luzi'
+    | '眼睛右_Luzi';
+
 declare namespace Translation {
-    type Entry = import('@sugarch/bc-asset-manager').Translation.Entry;
-    type Dialog = import('@sugarch/bc-asset-manager').Translation.Dialog;
-    type ActivityEntry = import('@sugarch/bc-asset-manager').Translation.ActivityEntry;
-    type CustomRecord<T extends string, U> = import('@sugarch/bc-asset-manager').Translation.CustomRecord<T, U>;
+    type Entry = import('@sugarch/bc-mod-types').Translation.Entry;
+    type Dialog = import('@sugarch/bc-mod-types').Translation.Dialog;
+    type ActivityEntry = import('@sugarch/bc-mod-types').Translation.ActivityEntry;
+    type GroupedEntries = import('@sugarch/bc-mod-types').Translation.GroupedEntries<CustomGroupBodyName>;
+    type CustomRecord<T extends string, U> = import('@sugarch/bc-mod-types').Translation.CustomRecord<T, U>;
 }
 
-type AssetOverrideContainer = import('@sugarch/bc-asset-manager').AssetOverrideContainer;
+type AssetOverrideContainer = import('@sugarch/bc-mod-types').AssetOverrideContainer;
 
 type CopyGroupInfo = { name: CustomGroupName; mirror: AssetGroupName; description?: Translation.Entry };
 
