43119aeaaa538b535a76a61cd96cec91a10d1b28
dynilath <dynilath@qq.com>
2025-03-20 09:53:50 +0800

    refactor: remove fp.js and convert log class to Logger object

--- src/log.js
+++ src/log.js
@@ -1,27 +1,13 @@
-import { ModInfo } from "./rollupHelper";
+import { ModInfo } from "@mod-utils/rollupHelper";
 
-export default class log {
-    /**
-     * 在控制台打印信息
-     * @param {string} message
-     */
-    static info(message) {
+export const Logger = {
+    info: (message) => {
         console.log(`[${ModInfo.name}] ${message}`);
-    }
-
-    /**
-     * 在控制台打印警告
-     * @param {string} message
-     */
-    static warn(message) {
+    },
+    warn: (message) => {
         console.warn(`[${ModInfo.name}] ${message}`);
-    }
-
-    /**
-     * 在控制台打印错误
-     * @param {string} message
-     */
-    static error(message) {
+    },
+    error: (message) => {
         console.error(`[${ModInfo.name}] ${message}`);
-    }
-}
+    },
+};
