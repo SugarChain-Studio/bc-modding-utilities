2cd6e2178b476cee1fe52bb27ed46e1d3a328fe5
dynilath <dynilath@qq.com>
2025-10-14 22:12:48 +0800

    refactor: remove createFollowLeadRunner method to streamline ChatRoomOrderTools

--- src/ChatRoomOrder/tools.js
+++ src/ChatRoomOrder/tools.js
@@ -56,52 +56,6 @@ class _ChatRoomOrderTools {
         ChatRoomLeashPlayer = from.MemberNumber;
         if (refresh !== false) CharacterRefreshLeash(Player);
     }
-
-    /**
-     * 生成一个跟随与引导的活动执行函数
-     * @param {[AssetGroupItemName, string]} followItem
-     * @param {[AssetGroupItemName, string]} leadItem
-     * @param {"prev"|"next"} leadMode
-     * @returns {CustomActivity["run"]}
-     */
-    createFollowLeadRunner(followItem, leadItem, leadMode) {
-        const follow = (C) =>
-            /** @type {PrevOrNextXCharacter}*/ ({
-                [leadMode === "next" ? "prevCharacter" : "nextCharacter"]:
-                    C.MemberNumber,
-            });
-        const lead = (C) =>
-            /** @type {PrevOrNextXCharacter}*/ ({
-                [leadMode === "next" ? "nextCharacter" : "prevCharacter"]:
-                    C.MemberNumber,
-            });
-
-        return (player, sender, { SourceCharacter, TargetCharacter }) => {
-            if (TargetCharacter === player.MemberNumber) {
-                if (!ServerChatRoomGetAllowItem(sender, player)) return;
-                Tools.findCharacter("SourceC", SourceCharacter)
-                    .then(() =>
-                        AssetGet("Female3DCG", followItem[0], followItem[1])
-                    )
-                    .then((asset, { SourceC }) =>
-                        this.wearAndPair(
-                            player,
-                            asset,
-                            follow(SourceC),
-                            "follow"
-                        )
-                    );
-            } else if (SourceCharacter === player.MemberNumber) {
-                Tools.findCharacter("TargetC", TargetCharacter)
-                    .then(() =>
-                        AssetGet("Female3DCG", leadItem[0], leadItem[1])
-                    )
-                    .then((asset, { TargetC }) =>
-                        this.wearAndPair(player, asset, lead(TargetC), "lead")
-                    );
-            }
-        };
-    }
 }
 
 export const ChatRoomOrderTools = new _ChatRoomOrderTools();
